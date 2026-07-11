9f6d848888b4b46d173ec3e91f19965063291d93
Egor <firstoff.egor@gmail.com>
2026-07-11 10:22:32 +0700

    Recognise_token recognizes all the new token types

--- recognizer.py
+++ recognizer.py
@@ -6,7 +6,17 @@ def recognise_token(buffer: str) -> Token | None:
         return
     if buffer == "\n":
         result.token_type = Tokentype.newline
-    if buffer in ("int", "str", "float", "bool"):
+    elif buffer == "function":
+        result.token_type = Tokentype.func
+    elif buffer == ",":
+        result.token_type = Tokentype.comma
+    elif buffer == "return":
+        result.token_type = Tokentype.ret
+    elif buffer == ":":
+        result.token_type = Tokentype.column
+    elif buffer == "->":
+        result.token_type = Tokentype.return_type
+    elif buffer in ("int", "str", "float", "bool"):
         result.token_type = Tokentype.type
     elif buffer in "+_**//%":
         result.token_type = Tokentype.aop
@@ -23,7 +33,7 @@ def recognise_token(buffer: str) -> Token | None:
 
 if __name__ == "__main__":
     assert recognise_token("") == None
-    assert recognise_token("\n") == None
+    assert recognise_token("\n").token_type == Tokentype.newline
     assert recognise_token("+").token_type == Tokentype.aop
     assert recognise_token("**").token_type == Tokentype.aop
     assert recognise_token("(").token_type == Tokentype.open_p
@@ -34,3 +44,5 @@ if __name__ == "__main__":
     assert recognise_token("None").token_type == Tokentype.literal
     assert recognise_token("=").token_type == Tokentype.binding
     assert recognise_token("my_var").token_type == Tokentype.variable_name
+    assert recognise_token("function").token_type == Tokentype.func
+    assert recognise_token(",").token_type == Tokentype.comma
